a3dfed77f98d276af09bba3372d6452143f9193c
Sinan Sevgi <sinansevgi@users.noreply.github.com>
2020-11-02 18:41:40 +0300

    Add spec files
    Test files added to project.

---
+++ spec/board_spec.rb
@@ -0,0 +1,75 @@
+# board.rb
+require '../lib/board'
+
+describe Board do
+  let(:board) { Board.new }
+  let(:value) { 'X' }
+  let(:pos) { [0, 1] }
+  describe 'Board#update' do
+    it 'Updates the board' do
+      board.update(value, pos)
+      board_test = board::board.flatten
+      expect(board_test).to eql(%w[_ X _ _ _ _ _ _ _])
+    end
+  end
+  describe 'Board#legal_input' do
+    let(:user_input) { 'A1' }
+    it 'Check users input is legal' do
+      expect(board.legal_input?('A1')).to eql(true)
+    end
+  end
+  describe 'Board#cell_empty?' do
+    it 'Check if the cell that user wants to modify is empty #Cell Empty' do
+      expect(board.cell_empty?([0,0])).to eql(true)
+    end
+  end
+  describe 'Board#cell_empty?' do
+    it 'Check if the cell that user wants to modify is empty #Cell Marked ' do
+      board.update(value, pos)
+      expect(board.cell_empty?([0, 1])).to eql(false)
+    end
+  end
+  describe 'Board#status' do
+
+    it 'Check if user won the game vertical' do
+      board.update(value, [0, 0])
+      board.update(value, [0, 1])
+      board.update(value, [0, 2])
+      expect(board.status).to eql(1)
+    end
+  end
+  describe 'Board#status' do
+
+    it 'Check if user won the game horizontal' do
+      board.update(value, [0, 0])
+      board.update(value, [1, 0])
+      board.update(value, [2, 0])
+      expect(board.status).to eql(1)
+    end
+  end
+  describe 'Board#status' do
+
+    it 'Check if user won the game diagonal' do
+      board.update(value, [0, 0])
+      board.update(value, [1, 1])
+      board.update(value, [2, 2])
+      expect(board.status).to eql(1)
+    end
+  end
+  describe 'Board#status' do
+
+    it 'Check if game is tie' do
+      board.update('X', [0, 0])
+      board.update('X', [1, 0])
+      board.update('O', [2, 0])
+      board.update('O', [0, 1])
+      board.update('O', [1, 1])
+      board.update('X', [2, 1])
+      board.update('X', [0, 2])
+      board.update('X', [1, 2])
+      board.update('O', [2, 2])
+      expect(board.status).to eql(0)
+    end
+  end
+
+end
